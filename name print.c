45b0fd74c5c8f6a2dee4e36e31636aefb4d5b333
AAKASHpdl <aakash.poudel10@gmail.com>
2016-11-14 12:42:43 +0545

    Add files via upload

---
+++ name print.c
@@ -0,0 +1,13 @@
+/*student name - aakash poudel
+ Date-2016-11-14*/
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    char name[10];
+printf("what is your name ??");
+scanf("%s",&name);
+printf ("your name is %s",name);
+getch();
+return 0;
+}
