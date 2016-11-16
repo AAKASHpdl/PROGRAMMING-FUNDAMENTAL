7ed2cc97f76de49beb7c23d49e7557a52a5e0551
AAKASHpdl <aakash.poudel10@gmail.com>
2016-11-16 12:55:40 +0545

    Add files via upload

---
+++ area of right angle triangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float a,b,c,s,d;
+printf ("enter any three number\n");
+scanf("%f%f%",&a,&b,&c);
+s=a+b+c;
+d = a*b*c;
+printf("sum of your number is :%f",s);
+prinf("product of your no is:%f",d);
+getch();
+return 0;
+}
+
