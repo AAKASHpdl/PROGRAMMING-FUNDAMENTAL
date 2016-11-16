5d454610e9e23efb78fbc2981eeb7175fdf21237
AAKASHpdl <aakash.poudel10@gmail.com>
2016-11-16 12:55:22 +0545

    Add files via upload

---
+++ sum and product.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float a,b,c,s,d;
+printf ("enter any three number\n");
+scanf("%f%f%f",&a,&b,&c);
+s=a+b+c;
+d = a*b*c;
+printf("sum of your number is :%f",s);
+printf("product of your no is:%f",d);
+
+getch();
+return 0;
+}
+
