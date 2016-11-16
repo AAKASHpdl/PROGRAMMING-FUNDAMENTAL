e828eba2fddb8197c2350418a3bfde4b86a1ffb7
AAKASHpdl <aakash.poudel10@gmail.com>
2016-11-16 12:23:24 +0545

    divide of two number

---
+++ divide.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float a,b,d;
+printf ("enter two number\n");
+scanf("%f%f",&a,&b);
+d=a/b;
+printf("a divided by b is :%f",d);
+getch();
+return 0;
+}
