3d427291bd73d8f42e4473f6cab659ea73d1a326
Aakash <aakash.poudel10@gmail.com>
2016-11-23 10:16:32 +0545

    Add files via upload

---
+++ convertion.c
@@ -0,0 +1,13 @@
+//student name:aakash poudel
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+float a,c;
+printf("enter the temperature in fahrenhit::");
+
+scanf("%f",&a);
+c=0.55*(a-32);
+printf("temperature in centigrade is :%f",c);
+
+}
---
+++ sum of digit of number.c
@@ -0,0 +1,13 @@
+//student name: aakash poudel
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,sum;
+printf("enter three numbers::\n");
+scanf("%d%d%d",&a,&b,&c);
+sum=a+b+c;
+printf("sum of three numbers are:%d",sum);
+
+}
