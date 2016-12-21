50bbf2874fcbfdaf5abce38fac46d993b094e635
Aakash <aakash.poudel10@gmail.com>
2016-12-21 10:23:05 +0545

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,17 @@
+//student name::aakash poudel
+#include<stdio.h>
+int main()
+{
+char a;
+printf("enter the alphabet\n");
+scanf("%c",&a);
+if (a=='a'||a=='e'||a=='i'||a=='O'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+{
+	printf ("the alphabet you entered is vowel sound");
+}
+else 
+{
+	printf("the alphabet you entered is not vowel sound");
+}
+return 0;
+}  
