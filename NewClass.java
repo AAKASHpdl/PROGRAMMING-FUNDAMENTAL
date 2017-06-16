31399fe9bea2655063665530758cc24817bbeef7
Aakash <aakash.poudel10@gmail.com>
2017-06-16 11:33:38 +0545

    Add files via upload

--- NewClass.java
+++ NewClass.java
@@ -1,4 +1,5 @@
 /*
+aakash poudel
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -17,9 +18,24 @@ public class NewClass {
 public NewClass()
 {}
 public NewClass(double a,double b, double c)
-{}
-
-fibd oots  
+{
+this.a= a;
+this.b=b;
+this.c=c;
+}
+public void find(){
+    this.discriminant =(this.b*this.b-4*this.a*this.c);
+}
+public void fiindRoots(){
+    if (this.discriminant >=0) {
+        double sqrtDis = Math.sqrt(this.discriminant);
+        this.root1=(((-1*this.b) + sqrtDis ) /(2*this.a));
+        this.root2 = (((-1* this.b)-sqrtDis)/(2*this.a));
+        system.out.println ("the root of the number is   
+        } else {
+        System.out.println("the roots doesnot exist.");
+        }}
+        
 
     }
 
