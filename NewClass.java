b00d0625a9c7640cdfe1710325c90af410643478
Aakash <aakash.poudel10@gmail.com>
2017-06-16 11:28:43 +0545

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author sunway
+ */
+public class NewClass {
+    private double a,b,c;
+    private double discriminant;
+    private double root1,root2;
+    
+public NewClass()
+{}
+public NewClass(double a,double b, double c)
+{}
+
+fibd oots  
+
+    }
+
+
+
